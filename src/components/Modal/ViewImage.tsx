f7606390481d0828da42ece57bf2abc27de2af43
Felipe P Covatti <felipepcovatti@gmail.com>
2021-05-29 19:38:01 -0300

    Implement image modal component (view image)

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,19 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay bgColor="blackAlpha.800" />
+      <ModalContent bgColor="pGray.800" maxW="fit-content" alignSelf="center">
+        <ModalBody p="0">
+          <Image src={imgUrl} maxH={600} maxW={900} mx="auto" />
+        </ModalBody>
+        <ModalFooter justifyContent="flex-start">
+          <Link href={imgUrl} isExternal>
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
